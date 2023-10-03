5dc11d362a5b8759ab8172af7ab98ecbbb580e05
Emil Kola <emil.kola@gmail.com>
2023-10-03 20:00:33 +0300

    Created LineController where to inherit from.

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    private LineRenderer lr;
+    private Transform[] points;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    public void SetUpLine(Transform[] points)
+    {
+        lr.positionCount = points.Length;
+        this.points = points;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            lr.SetPosition(i, points[i].position);
+        }
+
+    }
+}
