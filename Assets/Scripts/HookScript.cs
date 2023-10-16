4f5419c6da42699e32baa689c06eeac55a9d44f5
Aleksandar Hristov <uwutemka@gmail.com>
2023-10-16 18:12:10 +0300

    hook stops on space or mouse

--- Assets/Scripts/HookScript.cs
+++ Assets/Scripts/HookScript.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HookScript : MonoBehaviour
 {
+   
+
     private Rigidbody hook;
     public float hookSpeed = 1.0f;
 
+    
     private void OnCollisionEnter(Collision collision)
     {
         hook = GetComponent<Rigidbody>();
 
-        if (collision.gameObject.CompareTag("WaterPlane"))
-        {
-            hook.useGravity = false;
-            Vector3 upwardForce = Vector3.up * hookSpeed;
-            hook.AddForce(upwardForce, ForceMode.Impulse);
-
-        }
+        //if (collision.gameObject.CompareTag("WaterPlane"))
+        //{
+        //    hook.useGravity = false;
+        //    Vector3 upwardForce = Vector3.up * hookSpeed;
+        //    hook.AddForce(upwardForce, ForceMode.Impulse);
+        //
+        //}
 
     }
 }
