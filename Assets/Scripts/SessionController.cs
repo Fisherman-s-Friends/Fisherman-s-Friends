a3b34e301b27ad68612e94628ff5435e8165bf43
jaakop <jaakko@kronogra.fi>
2023-10-27 15:07:47 +0300

    Money counter and api for paying back

--- Assets/Scripts/SessionController.cs
+++ Assets/Scripts/SessionController.cs
@@ -1,12 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SessionController : MonoBehaviour
 {
     static private GameObject sessionObject;
 
+    [SerializeField] private int loan;
     [SerializeField] private int money;
+
+    [SerializeField]
+    private int paybackRate;
     void Start()
     {
         if (sessionObject != null)
@@ -16,13 +20,56 @@ public class SessionController : MonoBehaviour
         }
 
         sessionObject = gameObject;
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(sessionObject);
+
+        UpdateMoneyText();
     }
 
     void Update()
     {
-        
+            
     }
 
-    
+    public void AddMoney(int amount)
+    {
+        money += amount;
+        UpdateMoneyText();
+    }
+
+    public void NextDay()
+    {
+        money -= paybackRate;
+        if (money < 0)
+        {
+            throw new NotImplementedException("Not enough money, you lose");
+            return;
+        }
+        loan -= paybackRate;
+        if (loan <= 0)
+        {
+            throw new NotImplementedException("Loan payed, you win");
+            return;
+        }
+
+        UpdateMoneyText();
+    }
+
+    private void UpdateMoneyText()
+    {
+        var text = GameObject.FindGameObjectWithTag("moneyText");
+        if (text != null)
+            text.GetComponent<TMPro.TMP_Text>().text = money + " fishes";
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 75, 50), "Add money"))
+        {
+            AddMoney(10);
+        }
+        if (GUI.Button(new Rect(100, 10, 75, 50), "Next day"))
+        {
+            NextDay();
+        }
+    }
 }
