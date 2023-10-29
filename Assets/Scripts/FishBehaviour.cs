ae1ebb0b15946eb5b76b7d3966ce85b212b81a58
Emil Kola <emil.kola@gmail.com>
2023-10-29 12:15:13 +0200

    Edited defaults, added BarSize for more variance.

--- Assets/Scripts/FishBehaviour.cs
+++ Assets/Scripts/FishBehaviour.cs
@@ -8,13 +8,14 @@ public class FishBehaviour : ScriptableObject
 {
     // Lower SPEED value = faster fish movement -> fish moves more often inside a time period
     // Lower SMOOTHNESS value = more "snappy" movement -> the time it takes to move from A to B
+    // Lower BARSIZE value = fish gets caught faster
 
     // DEFAULTS (for reference):
-    // Quick fish:      speed 20, smoothness 5
-    // Default fish:    speed 30, smoothness 15
-    // Lazy fish:       speed 60, smoothness 25
+    // Quick fish:      speed 10, smoothness 5, barsize 100
+    // Default fish:    speed 15, smoothness 5, barSize 150
+    // Lazy fish:       speed 20, smoothness 10, barSize 220
 
-    public float speed, smoothness;
+    public float speed, smoothness, barSize;
 
     public virtual float FishMoveSpeed()
     {
@@ -24,5 +25,10 @@ public class FishBehaviour : ScriptableObject
     public virtual float FishMoveSmoothness()
     {
         return smoothness;
+    }
+
+    public virtual float FishBarValueSize()
+    {
+        return barSize;
     }
 }
